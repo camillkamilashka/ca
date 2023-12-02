68a16b84b90aee34df7a0dbda31edcf5c875b26e
kamilla <kamilla@debian.com>
2023-12-02 20:27:21 +0500

    test3

---
+++ lab03 (копия)/lab03.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void reverseArray(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int findAndReverse(int arr[], int size, int searchElement) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (arr[i] == searchElement) {
+            reverseArray(arr, 0, i - 1);
+            reverseArray(arr, i + 1, size - 1);
+            return i;
+        }
+    }
+    reverseArray(arr, 0, size - 1);
+    return -1;
+}
+
+int main() {
+    int size, searchElement;
+    printf("Введите количество элементов массива: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    srand(time(0)); // Инициализация генератора случайных чисел
+
+    printf("Исходный массив:\n");
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 101 - 50; // Генерация случайного числа в диапазоне [-50, 50]
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Введите искомый элемент: ");
+    scanf("%d", &searchElement);
+
+    int index = findAndReverse(arr, size, searchElement);
+
+    if (index != -1) {
+        printf("Первое вхождение искомого элемента: %d\n", index);
+    } else {
+        printf("Искомый элемент не найден\n");
+    }
+
+    printf("Измененный массив:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
